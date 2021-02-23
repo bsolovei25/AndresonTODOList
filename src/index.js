be31b41c1e3b3cc815912c39ba96b1d0d04fc49a
bsolovei25 <bsolovei25@gmai.com>
2021-02-23 14:32:32 +0200

    US002: deleted all commented lines

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 class Task{
   constructor(args){
-    //if (ValidateValuesClass.validationOfUserInputs({'string':taskName, 'Date':[taskDate1, taskDate2]}) && 
     if (ValidateValuesClass.validationOfUserInputs({'string':args[0], 'date':[args[1], args[2]]}) && 
     ValidateValuesClass.validateIfEmpty()){
       this.taskName = args[0];
@@ -11,43 +10,12 @@ class Task{
 }
 
 
-
-// class ValidateValuesClass{
-//   static validationOfUserInputs(args){
-//     for (let key in args){
-//       if (args[key] instanceof Array){
-//         for (let k = 0; k < args[key].length; k++){
-//           if (!args[key][k] instanceof Date){
-//             throw `${args[key]} must be typed as ${key}`
-//           }
-//         }
-//       }
-//       if (!typeof args[key] == key){
-//         throw `${args[key]} must be typed as ${key}`
-//       }
-//     }
-//   }
-
-//   static validateIfEmpty(args){
-//     let taskName = document.getElementById('nameId').value
-//     let taskDate1 = document.getElementById('editDateStart').valueAsDate
-//     let taskDate2 = document.getElementById('editDateEnd').valueAsDate
-//     if (taskName.length == 0 && taskDate1.length == 0 && taskDate2.length == 0){
-//       throw `fields must not be empty`
-//     }
-    
-//   }
-
-  
-// }
-
 function getNameId() {
   return document.querySelector('#nameId')
 }
 
 
 function isFieldValid(){
-  //let taskName = document.getElementById('nameId')
   if (!getNameId().checkValidity() || getNameId().value == ''){
     alert('Name shouldnt have symbols')
     return
@@ -62,12 +30,8 @@ getNameId().addEventListener("keydown", function (e) {
 });
 
 document.getElementById('ConfirmChanges').addEventListener('click',function(e){
-  //let taskName = document.getElementById('nameId').value
-  //let taskDate1 = document.getElementById('editDateStart').valueAsDate
-  //let taskDate2 = document.getElementById('editDateEnd').valueAsDate 
   let inputFields = document.querySelectorAll("#nameId, #editDateStart, #editDateEnd")
   if (isFieldValid){
-    //let singleTask = new Task(taskName, taskDate1, taskDate2)
     let singleTask = new Task(inputFields)
   }
   
